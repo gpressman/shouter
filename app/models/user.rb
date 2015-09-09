e59b0486573b43840294269b1eb8f8605ac24cd5
Gabe Pressman <pressman4747@gmail.com>
2015-09-09 13:41:14 -0400

    more follower relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   has_many :shouts
 
-  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_user_relationships, foreign_key: :follower_id, class_name: 'FollowingRelationship'
   has_many :followed_users, through: :following_relationships
+
+  has_many :follower_relationships, foreign_key: :followed_user_id, class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
   
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
