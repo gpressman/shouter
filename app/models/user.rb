5f360de2348f915eb985cc18283ed55bab24ff83
Gabe Pressman <pressman4747@gmail.com>
2015-09-09 13:22:46 -0400

    made following relationships model and set up relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :shouts
+
+  has_many :following_relationships, foreign_key: :follower_id
+  has_many :followed_users, through: :following_relationships
+  
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
