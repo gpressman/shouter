36468a496014912569ad1728af466ad1b35bccc3
Gabe Pressman <pressman4747@gmail.com>
2015-09-15 19:31:01 -0400

    added following/unfollowing notices

--- app/controllers/following_relationships_controller.rb
+++ app/controllers/following_relationships_controller.rb
@@ -2,12 +2,12 @@ class FollowingRelationshipsController < ApplicationController
 	def create
 		user = User.find(params[:user_id])
 		current_user.follow(user)
-		redirect_to user
+		redirect_to user, notice: "following user" 	
 	end
 
 	def destroy
-		current_user.followed(user)
-		redirect_to(user)
+		current_user.unfollow(user)
+		redirect_to user, notice: "unfollowing user"
 	end
 
 	private
