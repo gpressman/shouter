bebddcf083e7d45fe2570ff7aadf24ba0f9d6020
Gabe Pressman <pressman4747@gmail.com>
2015-09-15 19:09:10 -0400

    follow button changed helper to include unfollow button, added following? method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ActiveRecord::Base
   
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def following? user
+  	followed_user_ids.include?(user.id)
+  end
 end
