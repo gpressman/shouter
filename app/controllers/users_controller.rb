b1639ae6d2bf9a2d3156935c0a050346e367f9e7
Gabe Pressman <pressman4747@gmail.com>
2015-09-15 19:04:26 -0400

    fixed bug in users and followed_users relationship, added index route to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
+
+  def index
+    @users = User.all
+  end
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
-  end
+  end 
 
   def new
     @user = User.new
