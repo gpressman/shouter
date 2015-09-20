471a59d42d29487c7af020bfcf7f0c3c2c5297dc
Gabe Pressman <pressman4747@gmail.com>
2015-09-19 21:43:40 -0400

    pulled out methods related to following from user model to concern/follow.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,6 @@
 class User < ActiveRecord::Base
+  include Concerns::Following 
   has_many :shouts
-
-  has_many :followed_user_relationships, 
-    foreign_key: :follower_id, 
-    class_name: 'FollowingRelationship'
-  has_many :followed_users, through: :followed_user_relationships
-
-  has_many :follower_relationships, 
-    foreign_key: :followed_user_id, 
-    class_name: 'FollowingRelationship'
-  has_many :followers, through: :follower_relationships
   
-  validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
 
-  def follow(user)
-  	followed_users << user
-  end
-
-  def unfollow(user)
-  	followed_users.delete(user)
-  end
-
-  def following?(user)
-  	followed_user_ids.include?(user.id)
-  end
 end
